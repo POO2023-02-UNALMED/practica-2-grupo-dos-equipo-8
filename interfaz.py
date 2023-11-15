b14c89518ce5bfcf8ea2d06ad890a84ed86ab874
Zuleto1231 <mzueta@unal.edu.co>
2023-11-14 19:43:38 -0500

    Finalización de pestaña de inicio y menu

--- interfaz.py
+++ interfaz.py
@@ -1,10 +1,16 @@
 from tkinter import *
+from PIL import Image, ImageTk
+
+
+
 
 ventana_inicio = Tk()
 ventana_inicio.geometry("1500x1000")
-
+ventana_inicio.title("Pasteleria DeliHorno")
+icono=PhotoImage(file="icono.png")
+ventana_inicio.iconphoto(True,icono)
 # Frame de bienvenida
-frame_bienvenida = Frame(ventana_inicio, bg="brown", width=500, height=300)
+frame_bienvenida = Frame(ventana_inicio, width=100, height=100,bg="green")
 frame_bienvenida.grid(column=0, row=0, padx=50, pady=10, sticky="nsew")
 
 imagen_bienvenida = PhotoImage(file="bienvenida.png")
@@ -12,15 +18,68 @@ label_imagen = Label(frame_bienvenida, image=imagen_bienvenida)
 label_imagen.grid(row=0, column=0, sticky="nsew")
 
 # Frame de biografías
-frame_biografias = Frame(ventana_inicio, bg="LightPink2", width=1000, height=300)
+frame_biografias = Frame(ventana_inicio, bg="LightPink2", width=900, height=300)
 frame_biografias.grid(column=1, row=0, padx=20, pady=10, sticky="nsew")
+
+# Frame de imágenes
+frame_imagenes = Frame(ventana_inicio, bg="cornsilk1", width=300, height=300)
+frame_imagenes.grid(column=1, row=1, padx=20, pady=10, sticky="nsew")
+
+#Frame de inicio
+frame_inicio = Frame(ventana_inicio, width=100, height=10,bg="bisque2")
+frame_inicio.grid(column=0, row=1, padx=50, pady=10, sticky="nsew")
+
+boton_inicio=Button(frame_inicio, text="INGRESAR",bg="AntiqueWhite2",width=30,height=5,font=("Arial",10,"bold"),bd=5,relief="raised")
+boton_inicio.place(relx=0.1,rely=0.6)
+
+ancho_inicio = 70
+alto_inicio = 70
+foto_inicio1 = ImageTk.PhotoImage(Image.open("foto_inicio1.png").resize((250, 100)))
+foto_inicio2 = ImageTk.PhotoImage(Image.open("foto_inicio2.png").resize((ancho_inicio, alto_inicio)))
+foto_inicio3 = ImageTk.PhotoImage(Image.open("foto_inicio3.png").resize((ancho_inicio, alto_inicio)))
+foto_inicio4 = ImageTk.PhotoImage(Image.open("foto_inicio4.png").resize((ancho_inicio, alto_inicio)))
+foto_inicio5 = ImageTk.PhotoImage(Image.open("foto_inicio5.png").resize((ancho_inicio, alto_inicio)))
+
+label_inicio1 = Label(frame_inicio, image=foto_inicio1)
+label_inicio2 = Label(frame_inicio, image=foto_inicio2)
+label_inicio3 = Label(frame_inicio, image=foto_inicio3)
+label_inicio4 = Label(frame_inicio, image=foto_inicio4)
+label_inicio5 = Label(frame_inicio, image=foto_inicio5)
+
+label_inicio1.place(relx=0.1, rely=0.3)
+label_inicio2.place(relx=0, rely=0)
+label_inicio3.place(relx=0.8, rely=0)
+label_inicio4.place(relx=0.3, rely=0)
+label_inicio5.place(relx=0.5, rely=0)
+
+
+
+
+
+# Imágenes de Manuel
+foto_manuel1 = Image.open("foto1_manuel.png")
+foto_manuel2 = Image.open("foto2_manuel.png")
+foto_manuel3 = Image.open("foto3_manuel.png")
+foto_manuel4 = Image.open("foto4_manuel.png")
+
+# Cambiar el tamaño de las imágenes (ajustar según sea necesario)
+ancho = 200
+alto = 150
+foto_manuel1 = ImageTk.PhotoImage(foto_manuel1.resize((ancho, alto)))
+foto_manuel2 = ImageTk.PhotoImage(foto_manuel2.resize((ancho, alto)))
+foto_manuel3 = ImageTk.PhotoImage(foto_manuel3.resize((ancho, alto)))
+foto_manuel4 = ImageTk.PhotoImage(foto_manuel4.resize((ancho, alto)))
 
 # Datos de las biografías
 biografias = [
-    {"nombre": "Manuel Zuleta Arango", "correo": "mzueta@unal.edu.co", "cedula": "1001420813"},
-    {"nombre": "Nombre2 Apellido2", "correo": "correo2@dominio.com", "cedula": "0987654321"},
-    {"nombre": "Nombre3 Apellido3", "correo": "correo3@dominio.com", "cedula": "9876543210"},
-    {"nombre": "Nombre4 Apellido4", "correo": "correo4@dominio.com", "cedula": "1234567890"}
+    {"nombre": "Manuel Zuleta Arango", "correo": "mzueta@unal.edu.co", "cedula": "1001420813",
+     "carrera": "Ingeniería de Sistemas", "edad": 21, "semestre": 9},
+    {"nombre": "Nombre2 Apellido2", "correo": "correo2@dominio.com", "cedula": "0987654321",
+     "carrera": "Ciencias de la Computación", "edad": 22, "semestre": 6},
+    {"nombre": "Nombre3 Apellido3", "correo": "correo3@dominio.com", "cedula": "9876543210",
+     "carrera": "Ingeniería Electrónica", "edad": 24, "semestre": 7},
+    {"nombre": "Nombre4 Apellido4", "correo": "correo4@dominio.com", "cedula": "1234567890",
+     "carrera": "Ingeniería Industrial", "edad": 23, "semestre": 6}
 ]
 
 # Variable para rastrear la biografía actual
@@ -37,28 +96,79 @@ def actualizar_biografia():
     nombre = biografias[biografia_actual]["nombre"]
     correo = biografias[biografia_actual]["correo"]
     cedula = biografias[biografia_actual]["cedula"]
+    carrera = biografias[biografia_actual]["carrera"]
+    edad = biografias[biografia_actual]["edad"]
+    semestre = biografias[biografia_actual]["semestre"]
 
     for widget in frame_biografias.winfo_children():
         widget.destroy()
 
-    biografia = crear_seccion_biografia(frame_biografias, nombre, correo, cedula)
+    biografia = crear_seccion_biografia(frame_biografias, nombre, correo, cedula, carrera, edad, semestre)
     biografia.place(relx=0.5, rely=0.5, anchor="center")
     biografia.bind("<Button-1>", lambda event: cambiar_biografia())
 
+    # Mostrar las imágenes correspondientes en el frame_imagenes
+    mostrar_imagenes()
+
 # Función para crear la sección de biografía para cada creador
-contador = 0
-def crear_seccion_biografia(frame, nombre, correo, cedula):
-    global contador
-    seccion = LabelFrame(frame, text=nombre, font=("Arial", 10, "bold"), bg="LightPink2", padx=10, pady=5)
-    Label(seccion, text="Correo: ", bg="LightPink2").grid(row=0, column=0, sticky="w")
-    Label(seccion, text="Cédula: ", bg="LightPink2").grid(row=1, column=0, sticky="w")
-    Label(seccion, text=correo, bg="LightPink2").grid(row=0, column=1, sticky="w")
-    Label(seccion, text=cedula, bg="LightPink2").grid(row=1, column=1, sticky="w")
-    if contador == 3:
-        contador = 0
+def crear_seccion_biografia(frame, nombre, correo, cedula, carrera, edad, semestre):
+    seccion = LabelFrame(frame, text=nombre, font=("Arial", 25, "bold"), bg="LightPink2", padx=10, pady=5)
+    Label(seccion, text="Correo: ", bg="LightPink2", font=("Arial", 12)).grid(row=0, column=0, sticky="w")
+    Label(seccion, text="Cédula: ", bg="LightPink2", font=("Arial", 12)).grid(row=1, column=0, sticky="w")
+    Label(seccion, text="Carrera: ", bg="LightPink2", font=("Arial", 12)).grid(row=2, column=0, sticky="w")
+    Label(seccion, text="Edad: ", bg="LightPink2", font=("Arial", 12)).grid(row=3, column=0, sticky="w")
+    Label(seccion, text="Semestre: ", bg="LightPink2", font=("Arial", 12)).grid(row=4, column=0, sticky="w")
+    Label(seccion, text=correo, bg="LightPink2", font=("Arial", 12)).grid(row=0, column=1, sticky="w")
+    Label(seccion, text=cedula, bg="LightPink2", font=("Arial", 12)).grid(row=1, column=1, sticky="w")
+    Label(seccion, text=carrera, bg="LightPink2", font=("Arial", 12)).grid(row=2, column=1, sticky="w")
+    Label(seccion, text=str(edad), bg="LightPink2", font=("Arial", 12)).grid(row=3, column=1, sticky="w")
+    Label(seccion, text=str(semestre), bg="LightPink2", font=("Arial", 12)).grid(row=4, column=1, sticky="w")
     return seccion
+
+# Función para mostrar las imágenes en el frame_imagenes
+def mostrar_imagenes():
+    # Limpiar el frame_imagenes
+    for widget in frame_imagenes.winfo_children():
+        widget.destroy()
+
+    # Mostrar las imágenes correspondientes
+    if biografia_actual == 0:  # Manuel
+        Label(frame_imagenes, image=foto_manuel1, bg="cornsilk1").grid(row=0, column=0, padx=10, pady=20, sticky="nsew")
+        Label(frame_imagenes, image=foto_manuel2, bg="cornsilk1").grid(row=0, column=1, padx=10, pady=20, sticky="nsew")
+        Label(frame_imagenes, image=foto_manuel3, bg="cornsilk1").grid(row=1, column=0, padx=10, pady=20, sticky="nsew")
+        Label(frame_imagenes, image=foto_manuel4, bg="cornsilk1").grid(row=1, column=1, padx=10, pady=20, sticky="nsew")
+
+        # Configurar el frame para que las filas y columnas se expandan al centro
+        frame_imagenes.grid_rowconfigure(0, weight=1)
+        frame_imagenes.grid_rowconfigure(1, weight=1)
+        frame_imagenes.grid_columnconfigure(0, weight=1)
+        frame_imagenes.grid_columnconfigure(1, weight=1)
 
 # Inicializar con la primera biografía
 actualizar_biografia()
+
+# Diseño de Menu
+# funciones
+# Label para la descripción
+
+
+def mostrar_descripcion():
+    frame_descripcion=Frame(frame_bienvenida).place(relx=0.2,rely=0)
+    descripcion = Label(frame_descripcion, text="", font=("Arial", 12), bg="white")
+    descripcion.grid(row=0,column=0)
+    
+    descripcion.config(text="Esta es la mejor app para administración pastelera de Colombia")
+
+def salir():
+    ventana_inicio.destroy()
+
+menubar = Menu(ventana_inicio)
+
+menu_principal = Menu(menubar, tearoff=0)
+menu_principal.add_command(label="Descripción", command=mostrar_descripcion)
+menu_principal.add_command(label="Salir", command=salir)
+menubar.add_cascade(label="Inicio", menu=menu_principal)
+
+ventana_inicio.config(menu=menubar)
 
 ventana_inicio.mainloop()
