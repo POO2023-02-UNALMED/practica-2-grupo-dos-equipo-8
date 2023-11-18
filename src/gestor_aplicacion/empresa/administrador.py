787061d40a83b89607be8772e5c8a12d1f690d5c
Manuel Zuleta Arango <mzueta@unal.edu.co>
2023-11-18 16:04:09 -0500

    Cambios para intentar arreglar el main, ingredientes no quiere funcionar

--- src/gestor_aplicacion/empresa/administrador.py
+++ src/gestor_aplicacion/empresa/administrador.py
@@ -5,6 +5,10 @@ from gestor_aplicacion.empresa.envio import Envio
 from gestor_aplicacion.empresa.fabrica import Fabrica
 from gestor_aplicacion.empresa.ingrediente import Ingrediente
 from gestor_aplicacion.empresa.bodega import Bodega
+from gestor_aplicacion.producto.torta import Tortas
+from gestor_aplicacion.producto.pastel_frito import PastelesFritos
+from gestor_aplicacion.producto.dona import Donas
+
 
 class Administrador:
     def __init__(self, bodega, caja, camiones, fabrica):
@@ -33,9 +37,9 @@ class Administrador:
             with open('administrador.pickle', 'rb') as file:
                 administrador = pickle.load(file)
             bodega = administrador.bodega
-            Envio.setListaEnvios(Deserializador.deserializarEnvios())
+           # Envio.setListaEnvios(Deserializador.deserializarEnvios())
             Ingrediente.setIngredientesDisponibles(Deserializador.deserializarIngredientes())
-            Camion.setCamiones(Deserializador.deserializarCamiones())
+            #Camion.setCamiones(Deserializador.deserializarCamiones())
             return administrador
         except FileNotFoundError:
             administrador = Administrador.crearTodo()
@@ -45,9 +49,137 @@ class Administrador:
 
     @staticmethod
     def crearTodo():
-        bodega = Bodega("ABC123", productosInicialesBodega1, 1000, ingredientesInicialeBodega, productosIniciales, ingredientesIniciales)
+        #Listas de productos iniciales
+        ingredientes_iniciales_bodega = {}  
+        ingredientes_iniciales = []  
+
+        productos_iniciales_bodega = {}  
+        productos_iniciales = []  
+
+        productos_iniciales_bodega_1 = {}  
+        productos_iniciales = []  
+
+        #-----------------------------------------------------------------------------
+        # Crear una torta y agregar ingredientes a la bodega existente
+        ingredientes_torta = {}  
+
+        # Crear instancias de los ingredientes
+        harina = Ingrediente("harina", 5, 1234, 10)
+        azucar = Ingrediente("azúcar", 3, 1234, 5)
+
+        # Agregar los ingredientes al diccionario de ingredientes de la torta
+        ingredientes_torta[harina] = 1
+        ingredientes_torta[azucar] = 1
+
+        # Crear la torta con los ingredientes
+        torta = Tortas("torta", 5, ingredientes_torta, 0, "abc123", 3, 6, "chocolate")
+
+        # Agregar la torta a la lista de productos iniciales
+        productos_iniciales.append(torta)
+
+        # Agregar la torta al diccionario de productos iniciales en la otra bodega
+        productos_iniciales_bodega_1[torta.nombre] = 1
+        #-----------------------------------------------------------------------------------------------------------------------
+
+        # Crear un pastel frito y agregar ingredientes a las estructuras existentes
+
+        ingredientes_pasteles_fritos = {}  # Diccionario para los ingredientes del pastel frito
+
+        # Crear instancias de los ingredientes
+        harina2 = Ingrediente("harina", 5, 1234, 10)
+        azucar2 = Ingrediente("azúcar", 3, 1234, 5)
+
+        # Agregar los ingredientes al diccionario de ingredientes de pasteles fritos
+        ingredientes_pasteles_fritos[harina2] = 2  # Agrega 2 unidades de harina al pastel frito
+        ingredientes_pasteles_fritos[azucar2] = 1  # Agrega 1 unidad de azúcar al pastel frito
+
+        # Crear el pastel frito con los ingredientes
+        pastel_frito = PastelesFritos("pastelFrito", 5, ingredientes_pasteles_fritos, 20, "dfg123", 3, False, "tomate")
+
+        # Agregar el pastel frito a la lista de productos iniciales
+        productos_iniciales.append(pastel_frito)
+
+        # Agregar el pastel frito al diccionario de productos iniciales en la otra bodega
+        productos_iniciales_bodega_1[pastel_frito.nombre] = 1
+
+        #------------------------------------------------------------------------------------------------------------------------------------
+        # Crear unas galletas y agregar ingredientes a las estructuras existentes
+
+        ingredientes_galletas = {}  # Diccionario para los ingredientes de las galletas
+
+        # Crear instancias de los ingredientes
+        harina3 = Ingrediente("harina", 5, 1234, 10)
+        azucar3 = Ingrediente("azúcar", 3, 1234, 5)
+
+        # Agregar los ingredientes al diccionario de ingredientes de las galletas
+        ingredientes_galletas[harina3] = 2  # Agrega 2 unidades de harina a las galletas
+        ingredientes_galletas[azucar3] = 1  # Agrega 1 unidad de azúcar a las galletas
+
+        # Crear las galletas con los ingredientes
+        galletas = Galletas("galleta", 5, ingredientes_galletas, 20, "dfg123", 3, False, "vainilla")
+
+        # Agregar las galletas a la lista de productos iniciales
+        productos_iniciales.append(galletas)
+
+        # Agregar las galletas al diccionario de productos iniciales en la otra bodega
+        productos_iniciales_bodega_1[galletas.nombre] = 1
+
+        #---------------------------------------------------------------------------------------------------------------------------------
+        # Crear una dona y agregar ingredientes a las estructuras existentes
+
+        ingredientes_donas = {}  # Diccionario para los ingredientes de las donas
+
+        # Crear instancias de los ingredientes
+        harina4 = Ingrediente("harina", 5, 1234, 10)
+        azucar4 = Ingrediente("azúcar", 3, 1234, 5)
+
+        # Agregar los ingredientes al diccionario de ingredientes de las donas
+        ingredientes_donas[harina4] = 2  # Agrega 2 unidades de harina a las donas
+        ingredientes_donas[azucar4] = 1  # Agrega 1 unidad de azúcar a las donas
+
+        # Crear la dona con los ingredientes
+        dona = Donas("dona", 5, ingredientes_donas, 20, "dfg123", 3, False, "arequipe")
+
+        # Agregar la dona a la lista de productos iniciales
+        productos_iniciales.append(dona)
+
+        # Agregar la dona al diccionario de productos iniciales en la otra bodega
+        productos_iniciales_bodega_1[dona.nombre] = 1
+        #----------------------------------------------------------------------------------------------------------------------------------------
+        # Bucle para agregar ingredientes iniciales a la lista existente
+        for i in range(1, 11):
+            ingrediente1 = Ingrediente("harina", 2, i, 1)
+            ingredientes_iniciales.append(ingrediente1)
+            ingrediente2 = Ingrediente("azucar", 1, i + 10, 1)
+            ingredientes_iniciales.append(ingrediente2)
+            ingrediente3 = Ingrediente("huevos", 3, i + 20, 2)
+            ingredientes_iniciales.append(ingrediente3)
+            ingrediente4 = Ingrediente("levadura", 2, i + 30, 1)
+            ingredientes_iniciales.append(ingrediente4)
+            ingrediente5 = Ingrediente("mantequilla", 4, i + 40, 2)
+            ingredientes_iniciales.append(ingrediente5)
+        for ingrediente in ingredientes_iniciales:
+            ingredientes_iniciales_bodega[ingrediente.nombre] = 25
+
+
+
+        bodega = Bodega("ABC123", productos_iniciales_bodega_1, 1000, ingredientes_iniciales_bodega, productos_iniciales, ingredientes_iniciales)
         fabrica = Fabrica("CBS321", "CRR52#75", bodega)
-        
+                # Configurar una producción diaria inicial
+        produccion_diaria = {
+            torta: 1,
+            pastel_frito: 1,
+            dona: 1,
+            galletas: 1
+        }
+
+        # Establecer la producción diaria en la fábrica
+        fabrica.set_produccion_diaria(produccion_diaria)
+
+        # Finalizar la producción dos veces
+        fabrica.finalizar_produccion(produccion_diaria)
+        fabrica.finalizar_produccion(produccion_diaria)
+        caja=Caja(10000)
 
         administrador = Administrador(bodega, caja, Camion.getCamiones(), fabrica)
         return administrador
